f164002bced49ce5f949f05600dd83aa277b5c74
zsj <zuoshj@shanghaitech.edu.cn>
2018-12-13 21:04:25 +0800

    12

--- private/cs/hw_5.py
+++ private/cs/hw_5.py
@@ -1,103 +1,96 @@
 class program(object):
-    codes="a"
-    def __init__(self,codes):
-        self.codes=codes
-    
-    def CFG(self):
-        codes=self.codes;
-        a=[0 for i in range(100)]
-        b=["" for i in range(100)]
-        index=0
-        n=0
-        for i in range(codes.count("\n",0,len(codes))):
-            split=codes.split("\n")[i]
-            if split.find("{",0,len(split))>0:
-                start=i+1
-        for i in range(codes.count("\n",0,len(codes))):
-            split=codes.split("\n")[i]  
-            if split.find("}",0,len(split))>0:
-                end=i      
-        for i in range(start,end):
-            split=codes.split("\n")[i]
-            subsplit=codes.split("\n")[i+1]
-            if split.find("fi",0,len(split))<=0 and split.find("done",0,len(split))<=0:
-                index+=1
-            if i==start:
-                a[n]=index
-                b[n]="begin"
-                n+=1
-            if split.find("if",0,len(split))>0:
-                if subsplit.find("return",0,len(subsplit)<0):
-                    a[n]=index+1
-                    b[n]="if"
-                    n+=1
-            if split.find("while",0,len(split))>0:
-                a[n]=index
-                b[n]="while"
-                a[n+1]=index+1
-                n+=2
-            if split.find("if",0,len(split))>0 and subsplit.find("return",0,len(subsplit))<0:
-                pass
-            if split.find("done",0,len(split))>0 and subsplit.find("return",0,len(subsplit))<0:
-                    a[n]=index+1
-                    n+=1
-            if split.find("return",0,len(split))>0:
-                    a[n]=index
-                    b[n]="return"
-                    n+=1
-        matrix=[[0 for i in range(n)] for i in range(n)]
-        for i in range(n-1):
-            if b[i]=="if":
-                matrix[i-1][i]=1
-                matrix[i][i+1]=1
-                matrix[i-1][i+1]=1
-            elif b[i]=="while":
-                matrix[i-1][i]=1
-                matrix[i][i-1]=1
-            else :matrix[i][i+1]=1
-        order=[str(a[i])for i in range(n)]
-        order.sort()
+    codes = "a"
 
-        for j in range(n):
-            matrix[2][j]=matrix[n-1][j]
-        for i in range(2,n):
-            for j in range(n):
-                matrix[i][j]=matrix[i-1][j]
-        print("[",end="")
+    def __init__(self, codes):
+        self.codes = codes
+
+    def CFG(self):
+        codes = self.codes
+        a = [0 for i in range(100)]
+        b = ["" for i in range(100)]
+        index = 0
+        n = 0
+        for i in range(codes.count("\n", 0, len(codes))):
+            split = codes.split("\n")[i]
+            if "{" in split:
+                start = i+1
+        for i in range(codes.count("\n", 0, len(codes))):
+            split = codes.split("\n")[i]
+            if "}" in split:
+                end = i
+        for i in range(start, end):
+            split = codes.split("\n")[i]
+            subsplit = codes.split("\n")[i+1]
+            if "fi" not in split and "done" not in split:
+                index += 1
+            if i == start:
+                a[n] = index
+                b[n] = "begin"
+                n += 1
+            if "if" in split:
+                if "return" not in subsplit:
+                    a[n] = index+1
+                    b[n] = "if"
+                    n += 1
+            if "while" in split:
+                a[n] = index
+                b[n] = "while"
+                a[n+1] = index+1
+                n += 2
+            if "fi" in split and "return" not in subsplit:
+                a[n] = index+1
+                b[n] = "fi"
+                n += 1
+            if "done" in split and "return" not in subsplit:
+                a[n] = index+1
+                n += 1
+            if "return" in split:
+                a[n] = index
+                b[n] = "return"
+                n += 1
+        matrix = [[0 for i in range(n)] for i in range(n)]
+        for i in range(n-1):
+            if b[i] == "if":
+                matrix[i-1][i] = 1
+                matrix[i][i+1] = 1
+                matrix[i-1][i+1] = 1
+            elif b[i] == "while":
+                matrix[i][i+1] = matrix[i+1][i] = 1
+                if b[i+2] == "return":
+                    matrix[i][i+2] = 1
+            elif b[i] == "return":
+                matrix[i-1][i] = 1
+            elif b[i] == "begin" or b[i] == "fi":
+                matrix[i][i+1] = 1
+
+        order = [str(a[i])for i in range(n)]
+        order.sort()
         for i in range(n):
             for j in range(n):
-                print(matrix[i][j],end="")
-                if j!=(n-1):
-                    print(",",end="")
-            if i!=n-1:
-                print(";",end="")
-        print ("]")
-        print(a)
+                if a[j] == int(order[i]):
+                    a[i], a[j] = a[j], a[i]
+                    for k in range(n):
+                        matrix[i][k], matrix[j][k] = matrix[j][k], matrix[i][k]
+                    for l in range(n):
+                        matrix[l][i], matrix[l][j] = matrix[l][j], matrix[l][i]
+                    break
+        print("[", end="")
+        for i in range(n):
+            for j in range(n):
+                print(matrix[i][j], end="")
+                if j != (n-1):
+                    print(",", end="")
+            if i != n-1:
+                print(";", end="")
+        print("]")
+s = ""
 
-
-
-
-s="""
-    bool x = True;
-    bool y = False;
-    bool z = True;
-    bool a = True;
-    main()
-    {
-    1:  x= !y;
-    2:  z= !x;
-    3:  if ( (x & y) | (! z) )
-    4:      y= !y;
-    5:      pass;
-        fi
-    6:  x=!y;
-    7:  z=!z;
-    8:  while ( ( x | y) & (a | z) )
-    9:      a=!y;
-    10:     y=!z;
-        done
-    11: return x;
-    } 
-"""
-p=program(s)
+while "}" not in s:
+    s += input()
+    s += '\n'
+""" stopword = '    } '
+for line in iter(input, stopword): # 输入为空行，表示输入结束
+  s += line + '\n'
+  s+='    } ' """
+p = program(s)
 p.CFG()
