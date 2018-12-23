cd76c441d174002fe6b9c6c47f4dff9ba5e9ca94
zsj <zuoshj@shanghaitech.edu.cn>
2018-12-23 18:04:46 +0800

    123

--- private/cs/hw_5.py
+++ private/cs/hw_5.py
@@ -1,9 +1,23 @@
+import re
 class program(object):
     def __init__(self, codes):
         self.codes = codes
 
     def CFG(self):
         codes = self.codes
+        """ codes=codes.replace("if("," if (")
+        codes=codes.replace("while("," while (")
+        codes=codes.replace(":if",": if ")
+        codes=codes.replace(":while",": while ")
+        codes=codes.replace(";fi","; fi ")
+        codes=codes.replace("done\n"," done ")
+        codes=codes.replace("fi\n"," fi ") """
+        codes=re.sub(r"(?<=\;|\s)fi(?=((\d)+?\:))"," fi ",codes)
+        codes=re.sub(r"(?<=\:|\s)while(?=\(|\s)"," while ",codes)
+        codes=re.sub(r"(?<=\:|\s)if(?=\(|\s)"," if ",codes)
+        codes=re.sub(r"(?<=\;|\s)done(?=((\d)+?\:))"," done ",codes)
+        codes=re.sub(r"(?<=\:|\s)return(?=\s|\()"," return ",codes)
+
         a = [0 for i in range(100)]
         b = ["" for i in range(100)]
         index = 0
@@ -21,49 +35,49 @@ class program(object):
             after = codes.split(";")[i+1]
             before = codes.split(";")[i-1]
             index += 1
-            if i == start and ("if" not in split and "while" not in split):
+            if i == start and (" if " not in split and " while " not in split)and "return" not in split:
                 a[n] = index
                 b[n] = "begin"
                 n += 1
-            if "if" in split:
-                if i == start or ("fi"in split or "done"in split ):
+            if " if " in split:
+                if i == start or (" fi "in split or " done "in split ):
                     a[n] = index
                     b[n] = "if"
                     n += 1
                 index += 1
-                if "return" not in split :
+                if " return " not in split :
                     a[n] = index
                     b[n] = "if2"
                     n += 1
-            if "fi" in split and "return" not in split:
-                if i != end-1 and ("if" not in split and "while"not in split):
+            if " fi " in split and " return " not in split:
+                if i != end-1 and (" if " not in split and " while "not in split):
                     a[n] = index
                     b[n] = "fi"
                     n += 1
-            if "while" in split:
+            if " while " in split:
                 a[n] = index
                 b[n] = "while"
                 index += 1
                 n+=1
-                if "return" not in split:
+                if  " return " not in split:
                     a[n] = index
                     b[n] = "while2"
                     n += 1
-            if "done" in split and "return" not in split :
-                if i != end-1 and ("if" not in after and "while"not in after):
+            if " done " in split and " return " not in split :
+                if i != end-1 and (" if " not in split and " while "not in split):
                     a[n] = index
                     b[n] = "done"
                     n += 1
-            if "return" in split:
-                if ("done"not in after and"fi"  not in after):
+            if " return " in split:
+                if (" done "not in after and" fi "  not in after):
                     a[n] = index
                     b[n] = "last return"
                     n += 1
-                if "fi" in after:
+                if " fi " in after:
                     a[n] = index
                     b[n] = "if return"
                     n += 1
-                if "done" in after:
+                if " done " in after:
                     a[n] = index
                     b[n] = "while return"
                     n += 1
@@ -104,45 +118,23 @@ class program(object):
                         matrix[l][i], matrix[l][j] = matrix[l][j], matrix[l][i]
                     break
 
-        print("[", end="")
-        for i in range(n):
-            for j in range(n):
-                print(matrix[i][j], end="")
-                if j != (n-1):
-                    print(",", end="")
-            if i != n-1:
-                print(";", end="")
-        print("]")
+        if n>1:
+            print("[", end="")
+            for i in range(n):
+                for j in range(n):
+                    print(matrix[i][j], end="")
+                    if j != (n-1):
+                        print(",", end="")
+                if i != n-1:
+                    print(";", end="")
+            print("]")
+        else:
+            print('[0]')
 
-s="""
-    bool x = True;
-    bool y = False;
-    bool z = True;
-    bool a = True;
-    main()
-    {
-    1:  x= !y;
-    2:  z= !x;
-    3:  if ( (x & y) | (! z) )
-    4:      y= !y;
-    5:      pass;
-        fi
-    6:  x=!y;
-    7:  z=!z;
-    8:  while ( ( x | y) & (a | z) )
-    9:      a=!y;
-    10:     y=!z;
-        done
-    11: return x;
-    } 
-"""
 """ s = ""
 while "}" not in s:
     s += input()
-    s += '\n' """
-""" stopword = '    } '
-for line in iter(input, stopword): # 输入为空行，表示输入结束
-  s += line + '\n'
-  s+='    } ' """
+    s += ' ' """
+s="main() { gs:if;}"
 p = program(s)
 p.CFG()
